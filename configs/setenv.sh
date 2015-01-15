2cc9343f34b6f82a87c98076fc9215ca107d40c8
Mike Sprague <mikesprague@gmail.com>
2015-01-15 15:59:39 -0500

    Fix paths

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -3,5 +3,5 @@
 JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx512m -XX:MaxPermSize=128m ";
 export JAVA_OPTS;
 
-CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=/webroot/WEB-INF/lib -Djava.library.path=/webroot/WEB-INF/railo/lib -javaagent:/sites/webroot/WEB-INF/lib/railo-inst.jar ";
+CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=/sites/webroot/WEB-INF/lib -Djava.library.path=/sites/webroot/WEB-INF/railo/lib -javaagent:/sites/webroot/WEB-INF/lib/railo-inst.jar ";
 export CATALINA_OPTS;
